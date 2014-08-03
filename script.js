a94bfaf5482e88d0865419ac601b8019d00c3682
dcraven <dcravenus@gmail.com>
2014-08-02 22:47:38 -0600

    Added descriptions and fixed markup a bit

--- script.js
+++ script.js
@@ -3,3 +3,18 @@ $.backstretch("field.jpg");
 var toggleInfo = function(){
   $('#info-tag').toggle();
 };
+
+var toggleWormDescription = function(){
+  $('#apeworm-description').slideToggle();
+  $('#worm-chevron').toggleClass("fa-chevron-down fa-chevron-up");
+}
+
+var toggleHummediaDescription = function(){
+  $('#hummedia-description').slideToggle();
+  toggleChevron($('#hummedia-chevron'));
+}
+
+var toggleApeDescription = function(){
+  $('#ape-description').slideToggle();
+  toggleChevron($('#ape-chevron'));	
+}
